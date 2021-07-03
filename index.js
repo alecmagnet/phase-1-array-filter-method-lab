cb0d2511e5a2102356fbe6442ebaec7b0eb14b5d
Alec Magnet <amagnet@gmail.com>
2021-07-03 14:47:32 -0400

    commit tree 29ddf82cbdf4d1a7f8f805e1ffd070b445b5a486

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution here
+const drivers = [
+	{
+		name: 'Bobby',
+		hometown: 'Pittsburgh'
+	},
+	{
+		name: 'Sammy',
+		hometown: 'New York'
+	},
+	{
+		name: 'Sally',
+		hometown: 'Cleveland'
+	},
+	{
+		name: 'Annette',
+		hometown: 'Los Angeles'
+	},
+	{
+		name: 'Bobby',
+		hometown: 'Tampa Bay'
+	}
+];
+
+expect(matchName(drivers, 'Bobby')).to.eql([
+	{
+		name: 'Bobby',
+		hometown: 'Pittsburgh'
+	},
+	{
+		name: 'Bobby',
+		hometown: 'Tampa Bay'
+	}
+]);
+
+function findMatching (namesArray, string) {
+	return namesArray.filter((num) => num.toLowerCase() === string.toLowerCase())
+};
+
+function fuzzyMatch(namesArray, string) {
+	return namesArray.filter((num) => num[0] === string[0])
+};
+
+function matchName(namesArray, string) {
+	return namesArray.filter((num) => num['name'] === string)
+};
